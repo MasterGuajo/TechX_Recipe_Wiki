781d19a0d3c53eee13810cb141de934a54263a27
Rodrigo Lopes <rodrigo_c_lopes@techexchange.in>
2023-02-23 23:50:00 +0000

    Added routing, implemented page data retrieval in Backend, did some basic templating for testing

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,14 +1,30 @@
+from google.cloud import storage
+import json
+
 # TODO(Project 1): Implement Backend according to the requirements.
 class Backend:
 
     def __init__(self):
         pass
         
-    def get_wiki_page(self, name):
-        pass
+    #changed to check id instead of name
+    def get_wiki_page(self, id):
+        storage_client = storage.Client()
+        blobs = storage_client.list_blobs("nrjcontent")
+        for blob in blobs:
+            page_data = json.loads(blob.download_as_string(client=None))
+            if id == int(page_data["page_id"]):
+                return page_data
 
-    def get_all_page_names(self):
-        pass
+    #changed to return full list of page data instead of names
+    def get_all_pages(self):
+        storage_client = storage.Client()
+        blobs = storage_client.list_blobs("nrjcontent")
+        all_pages_data = []
+        for blob in blobs:
+            page_data = json.loads(blob.download_as_string(client=None))
+            all_pages_data.append(page_data)
+        return all_pages_data
 
     def upload(self):
         pass
--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,14 +1,34 @@
 from flask import render_template
 
+from flaskr.backend import Backend
 
+"""
+run cmds:
+
+cd project
+chmod a+x run-flask.sh
+./run-flask.sh
+"""
 def make_endpoints(app):
 
-    # Flask uses the "app.route" decorator to call methods when users
+    # Flask uses the "app.route" decorator tos call methods when users
     # go to a specific route on the project's website.
     @app.route("/")
+    @app.route("/home")
+    @app.route("/index")
     def home():
-        # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the home page.
         return render_template("main.html")
+
+    @app.route("/pages")
+    def pages():
+        return render_template("pages.html", pages=Backend.get_all_pages(0))
+
+    @app.route("/pages/<int:page_id>")
+    def show_page(page_id):
+        return render_template("page.html", page_data=Backend.get_wiki_page(0,page_id))
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
