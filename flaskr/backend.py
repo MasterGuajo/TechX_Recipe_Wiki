e6f97b0bda92e5160df0c926946c215bd84c47c8
Nicole Roman <nicole.roman@techexchange.in>
2023-02-28 03:08:31 +0000

    SignUp and SignIn work, missing HTML requirements.

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,3 +1,6 @@
+from google.cloud import storage
+import json
+
 # TODO(Project 1): Implement Backend according to the requirements.
 class Backend:
 
@@ -13,11 +16,31 @@ class Backend:
     def upload(self):
         pass
 
-    def sign_up(self):
-        pass
+    def sign_up(self, new_user, username):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("userpass")
+        blob = bucket.blob(username)
+        with blob.open("w") as f:
+            f.write(json.dumps(new_user))
+        return storage.Blob(bucket=bucket, name=username).exists(storage_client)
 
-    def sign_in(self):
-        pass
+    def sign_in(self, existing_user, username):
+        '''storage_client = storage.Client()
+        blobs = storage_client.list_blobs("userpass")
+        for blob in blobs:
+            if json.dumps(existing_user) == blob.download_as_string(client=None):
+                return True
+            else:
+                return False'''
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("userpass")
+        blob = bucket.blob(username)
+        if blob.exists():
+            return True
+        else:
+            return False
+
+
 
     def get_image(self):
         pass
--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,6 +1,6 @@
-from flask import render_template
-
-
+from flask import render_template, request, flash, redirect
+from flaskr.backend import Backend
+import hashlib
 def make_endpoints(app):
 
     # Flask uses the "app.route" decorator to call methods when users
@@ -10,5 +10,37 @@ def make_endpoints(app):
         # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
         # to render main.html on the home page.
         return render_template("main.html")
+
+    @app.route("/signup")
+    def signup():
+        return render_template("signup.html")
+
+    @app.route("/check_signup", methods=['POST'])
+    def check_signup():
+        user_name = request.form["Username"]
+        password = "prefix" + request.form["Password"] 
+        hash = hashlib.blake2b(password.encode()).hexdigest()
+        user = {user_name : str(hash)}
+        if Backend.sign_up(None,user, user_name):
+            return redirect('/')
+        else:
+            return "error"
+        
+
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
+
+    @app.route("/check_login", methods=['POST'])
+    def check_login():
+        user_name = request.form["Username"]
+        password = "prefix" + request.form["Password"] 
+        hash = hashlib.blake2b(password.encode()).hexdigest()
+        user = {"User" : user_name, "Pass" : str(hash)}
+        if Backend.sign_in(None,user, user_name):
+            return redirect('/')
+        else: 
+            return "error"
+
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
