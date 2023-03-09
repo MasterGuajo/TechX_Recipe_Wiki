7d1c18a0b771720ce13cddb75b138474b029a080
Jorge Garcia <jorge_garcia@techexchange.in>
2023-03-09 02:13:04 +0000

    Added tests and comments

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -10,8 +10,10 @@ import json
 # TODO(Project 1): Implement Backend according to the requirements.
 class Backend:
 
-    def __init__(self):
-        pass
+    
+    def __init__(self, storage_client = storage.Client()):
+        self.storage_client = storage_client
+        
         
     #changed to check id instead of name
     def get_wiki_page(self, id):
@@ -34,16 +36,28 @@ class Backend:
 
     def upload(self,bucket_name,blob_name):
         
-        storage_client = storage.Client()
+        storage_client = storage.Client()        
 
         bucket = storage_client.bucket(bucket_name)
 
-        blob = bucket.blob(blob_name)
+        blob = bucket.blob("pages/" + blob_name)
 
         blob.upload_from_filename(blob_name)
 
         return
+    """ Uploads allowed files into bucket
 
+    After checking that its a valid file from pages.py, we take in the object we would
+    like to upload and specfiy the route in which our file will be stored
+
+    Args:
+        bucket_name: Shows us where the object will be stored
+        blob_name: The file that is going to be uploaded
+    
+    Returns:
+        Doesn't return anything as we are only communicating it with our backend
+
+    """
     def sign_up(self, new_user, username):
         storage_client = storage.Client()
         bucket = storage_client.bucket("userpass")
@@ -74,9 +88,22 @@ class Backend:
 
         storage_client = storage.Client()
         bucket = storage_client.bucket("nrjcontent")
+        # bucket = storage_client.bucket("nrjcontent/images/authors")
         blob = bucket.blob(blob_name)
 
         imageObj = blob.open('rb')
         imageBytes = imageObj.read()
  
         return base64.b64encode(imageBytes)
+
+    """ Retrieves images from Bucket
+
+    Retrieves images specified from pages.py from predetermined bucket
+    and encodes them so they can be used in HTML.
+
+    Args:
+        blob_name: Takes in name of files that are going to be retrieved. 
+        Ex: 'image.png'
+    Returns:
+        The Bytes of the image with base64 encoding.
+    """
--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,3 +1,45 @@
+import unittest
+
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
 from flaskr.backend import Backend
+from flask.testing import FlaskClient
+
+import io
+from io import BytesIO
 
 # TODO(Project 1): Write tests for Backend methods.
+
+def test_upload():
+
+    test_blob = MagicMock()
+    test_bucket = MagicMock()
+    test_storage_client = MagicMock()
+
+    test_storage_client.bucket.return_value = test_bucket
+    test_bucket.blob.return_value = test_blob
+
+    with patch('google.cloud.storage.Client', return_value = test_storage_client):
+        backend = Backend(test_storage_client)
+
+        backend.upload('nrjcontent', 'testfile.json')
+
+        test_blob.upload_from_filename.assert_called_once()
+""" Tests upload function from backend by asserting the method is called
+
+For this test we are mocking everything, from blob, to the bucket and storage client. We set every mocks return value to point to one 
+another so that they're chained.
+After this we are still patching in a storage client in order to "simulate" a backend.
+Once all of this is done, we can call upload and pass in our fake data.
+If the function which actually uploads data to our bucket is called, we know the function is working.
+
+Args:
+    We're not taking anything in directly for this test
+
+Uses: 
+    Simulated backend and backend functions
+
+Returns:
+    Our return statement is going to be whether or not our assertion passed or fail.
+"""
