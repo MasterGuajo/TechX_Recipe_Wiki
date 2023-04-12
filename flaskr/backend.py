3b7f11901d49101ab7f5596cc97986d26f985d96
Nicole Roman <nicole.roman@techexchange.in>
2023-04-12 00:41:00 +0000

    Added comments and unit tests.

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -157,7 +157,14 @@ class Backend:
     """
 
     def belongs_to_game(self, game_titles):
-        result_pages = set()
+        """Iterate through pages, evaluate if their game title
+            belongs to the list of titles the user chose.
+            Return all pages that match the game title.        
+
+        Args:
+          game_titles: List of game titles selected by the user.
+        """
+        result_pages = []
         storage_client = storage.Client()
         blobs = storage_client.list_blobs("nrjcontent",
                                           prefix="pages/",
@@ -166,11 +173,18 @@ class Backend:
             page_data = json.loads(blob.download_as_bytes(client=None))
             for game in game_titles:
                 if game.lower() == page_data["game"].lower():
-                    result_pages.add(page_data)
+                    result_pages.append(page_data)
         return result_pages
 
     def is_quick_enough(self, time_range):
-        result_pages = set()
+        """Iterate through pages, evaluate if their time
+            belongs to the time the user chose.
+            Return all pages that match the time.        
+
+        Args:
+          time_range: Int value representing minutes.
+        """
+        result_pages = []
         storage_client = storage.Client()
         blobs = storage_client.list_blobs("nrjcontent",
                                           prefix="pages/",
@@ -178,15 +192,19 @@ class Backend:
         for blob in blobs:
             page_data = json.loads(blob.download_as_bytes(client=None))
             if int(time_range) >= int(page_data["time"]):
-                result_pages.add(page_data)
+                result_pages.append(page_data)
         return result_pages
 
     def surprise_me(self):
+        """Returns a random page my using the random
+            module to select a randomized index from 
+            the list of all recipe pages.
+        """
         storage_client = storage.Client()
         blobs = list(storage_client.list_blobs("nrjcontent",
                                           prefix="pages/",
                                           delimiter="/"))
-        index = random.randint(len(blobs))
+        index = random.randint(0, len(blobs)-1)
         blob = blobs[index]
         page_data = json.loads(blob.download_as_bytes(client=None))
         return page_data
